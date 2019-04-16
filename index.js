5a1ff00e2f1c95e602411f9520b5e542f2dee858
Waescheklammer <33402322+Waescheklammer@users.noreply.github.com>
2019-04-16 11:27:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,12 +7,14 @@
 
 										TODO
 								*Promise für Installation + Button
-								*Button LösungenText
+								*Button LösungenText 
 								*Noten anzeigen/Mit Lösungen verbinden
 								*Desktop CSS Layout
 								*ASYNC Event listeners -> nicht synchron mit fetch
 									-> Hat keine daten, feuert bevor fetch geladen wird
 								*BUG:bei zweitem durchlauf komisches verhalten
+								*Service Worker(?)
+								*HTML Restrukturierunwqwg von Main
  */
 
 let menu = document.getElementById("main__menu");
@@ -43,7 +45,7 @@ function fetchRequest(addr){
 				localStorage.setItem("notes", JSON.stringify(data.notes));
 				localStorage.setItem("solution", JSON.stringify(data.solution));
 				insertFirstPage();
-				responseButtons();
+				
 			}).catch(err => {
 			  // Do something for an error here
 			});
@@ -88,6 +90,59 @@ function randomizeChoiceButtons(buttons, randomDec){
 //---------------------------
 
 
+function renderNotes(type){
+	//VF = Vex.Flow;
+
+	if(type===1){//Bassschlüssel
+
+	}
+	if(type===2){//Violinschlüssel
+
+	}
+	// Create an SVG renderer and attach it to the DIV element named "boo".
+	var canvas = document.getElementById("VexBody")
+	//var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+	var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
+
+	// Configure the rendering context.
+	renderer.resize(410, 150); //in CSS class für responsive 100%
+	var context = renderer.getContext();
+	context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+	// Create a stave of width 400 at position 10, 40 on the canvas.
+	var stave = new Vex.Flow.Stave(10, 40, 370); //Beeiflusst ebenfalls die Groeße
+
+	// Add a clef and time signature.
+	stave.addClef("treble");//.addTimeSignature("4/4"); //addClef("bass")
+
+	// Connect it to the rendering context and draw!
+	stave.setContext(context).draw();
+
+	var notes = [
+	  // A quarter-note C.
+	  new Vex.Flow.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
+
+	  // A quarter-note D.
+	  new Vex.Flow.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
+
+	  // A quarter-note rest. Note that the key (b/4) specifies the vertical
+	  // position of the rest.
+	  new Vex.Flow.StaveNote({clef: "treble", keys: ["b/4"], duration: "q" }),
+
+	  // A C-Major chord.
+	  new Vex.Flow.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
+	];
+
+	// Create a voice in 4/4 and add above notes
+	var voice = new Vex.Flow.Voice({num_beats: 4,  beat_value: 4});
+	voice.addTickables(notes);
+
+	// Format and justify the notes to 400 pixels.
+	var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 400);
+
+	// Render voice
+	voice.draw(context, stave);
+}
 //Rendert (später) Notenanzeige
 function renderRenderbody(solutionIndex){
 	let textArr=JSON.parse(localStorage.getItem("notes"));
@@ -108,6 +163,9 @@ function handleButton(event){
 	console.log(solutionIndex);
 	let lastPage = () => {
 		jP.innerHTML = Correct +"/4";
+		//let section ='<section id="main__statistic" class="hidden"><div id="statistic__container"><h2>Dein Ergebnis</h2><span id="Jens" style="text-align: center; font-size: 5rem"></span><button class="statistic__button" id="statistic__button">Zurück zum Menu </button></div></section>'
+		//document.getElementById("main").innerHTML=section;
+
 		quiz.classList.add("hidden");
 		stat.classList.remove("hidden");	
 	}
@@ -144,28 +202,36 @@ function handleButton(event){
 }
 
 function insertFirstPage(){
+	let section = '<section id="main__quiz" class=""><div class="quiz__progressbox"><progress id="progress" value="00" max="40" class="quiz__progressbar"></progress></div><div class="quiz__renderbody" id="quiz__renderbody"><p style="text-align:center; font-size:2rem;"  id="renderParagraph"></p><canvas id="renderbody"></canvas></div><hr><h2>Wählen sie eine Lösungsmöglichkeit aus</h2><div class="main__choicebox" id="main__choicebox"><button class="choiceButton1" id="choiceButton1">A</button><button class="choiceButton2" id="choiceButton2">B</button><button class="choiceButton3" id="choiceButton3">C</button><button class="choiceButton4" id="choiceButton4">D</button></div></section>'
+
+	//document.getElementById("main").innerHTML=section;
 	menu.classList.add("hidden");
 	quiz.classList.remove("hidden");
 	renderRenderbody(solutionIndex);
 	randomizeChoiceButtons(buttons, randomChoice);
+	responseButtons();
 }
 
 //fect('http://192.168.0.108:8000/Modul2.json');
 //Start Button
 document.getElementById("form_button").addEventListener('click', () =>{
 	var sel = document.getElementById("menuform").value;
+	const htwAddr1="https://www2.htw-dresden.de/~s77199/Modul1.json";
+	const htwAddr2="https://www2.htw-dresden.de/~s77199/Modul2.json";
 	const mod2Addr= 'http://192.168.0.108:8000/Modul2.json'; 
 	const mod1Addr= 'http://192.168.0.108:8000/Modul1.json'; 
 
 	if(sel === 'Bassschlüssel'){
 		localStorage.clear();
-		fetchRequest(mod2Addr);
+		fetchRequest(htwAddr2);
+		renderNotes(1);
 	}
 	if(sel === 'Violinschlüssel'){
 		localStorage.clear();
-		fetchRequest(mod1Addr);
+		fetchRequest(htwAddr1);
+		renderNotes(2);
 	}
-	if (sel !== 'Modul') {
+	if (sel === 'Modul') {
 		form.classList.add("main__choicebox--redLine");
 	}
 	
@@ -205,3 +271,21 @@ function responseButtons(){
 	}
 });
 }
+
+/*
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+*/
+
+
+
+
