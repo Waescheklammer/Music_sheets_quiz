adc5ddbd6f55cee6095877c090c13a055fe2d909
Waescheklammer <33402322+Waescheklammer@users.noreply.github.com>
2019-04-21 18:54:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -6,10 +6,16 @@
  --------------------------------------------------------------------------------------
 
 										TODO
-								*Promise für Installation + Button
-								*Button LösungenText
+								*Button LösungenText 
 								*Noten anzeigen/Mit Lösungen verbinden
-								*Desktop CSS Layout
+								*ASYNC Event listeners -> nicht synchron mit fetch
+									-> Hat keine daten, feuert bevor fetch geladen wird
+								*BUG:bei zweitem durchlauf komisches verhalten
+									->beim 1. durchlauf wird 1. Frage doppelt gerendert
+									-> bei folgenden Durchläufen überspringt schleife immer eine indizierung mehr
+										/ verdoppelt die vorherige
+									=> notfix : Seite beim Statistik Screen neuladen
+								*HTML Restrukturierunwqwg von Main
  */
 
 let menu = document.getElementById("main__menu");
@@ -27,7 +33,6 @@ let solutionIndex = 0; //Fragen/Antworten-Katalog Index
 let Correct=0; //Richtige Antwortenzähler
 
 
-
 //---------------------------
 //			FETCH JSON
 //---------------------------
@@ -40,15 +45,14 @@ function fetchRequest(addr){
 			}).then(data => {
 				localStorage.setItem("notes", JSON.stringify(data.notes));
 				localStorage.setItem("solution", JSON.stringify(data.solution));
+				insertFirstPage();
+				
 			}).catch(err => {
 			  // Do something for an error here
 			});
 		}
 
-
-fetchRequest(fetchAddr);
-
-
+//fetchRequest(fetchAddr);
 
 //---------------------------
 //		Render Buttons
@@ -71,13 +75,19 @@ randomChoice = shuffle(randomChoice);
 
 //Rendert Buttontexte zufällig an
 function randomizeChoiceButtons(buttons, randomDec){
-	let choices=["A", "B", "C", "D"];
+	let textSelection = ["C", "D", "E", "F", "G", "A", "B", "C'", "C#", "D#", "F#", "A#"];
+		textSelection = shuffle(textSelection);
+	let solution = JSON.parse(localStorage.getItem("solution"));
+	let choices = [];
+		choices[0] = solution[solutionIndex] +"_";
+		choices[1] = textSelection[0];
+		choices[2] = textSelection[1];
+		choices[3] = textSelection[2];
 	for(let i=0; i< buttons.length; ++i){
 		document.getElementById(buttons[i]).innerHTML=choices[randomDec[i]];
 	}	
 }
 
-randomizeChoiceButtons(buttons, randomChoice);
 
 
 
@@ -86,60 +96,62 @@ randomizeChoiceButtons(buttons, randomChoice);
 //		Note rendering
 //---------------------------
 
-//VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var canvas = document.getElementById("VexBody")
-//var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
+function renderNotes(type){
+		//Holt sich Clef aus dem Optionsmenü im HTML
+	
+	let noteType;
+	if(type==1){noteType="bass"}
+	if(type==2){noteType="treble"}
+	var clef = noteType;
+	console.log(clef);
+	var h = JSON.parse(localStorage.getItem("solution"));
 
-// Configure the rendering context.
-renderer.resize(410, 150); //in CSS class für responsive 100%
-var context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+	//var note = localStorage.getItem("notes");
 
-// Create a stave of width 400 at position 10, 40 on the canvas.
-var stave = new Vex.Flow.Stave(10, 40, 370); //Beeiflusst ebenfalls die Groeße
+	//lokale Definition bis Schnittstelle klar
+	var note = "g/4";
+	
+	// Create an SVG renderer and attach it to the DIV element named "boo".
+	var canvas = document.getElementById("VexBody")
+	//var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+	var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
 
-// Add a clef and time signature.
-stave.addClef("treble");//.addTimeSignature("4/4"); //addClef("bass")
+	// Configure the rendering context.
+	renderer.resize(410, 150); //in CSS class für responsive 100%
+	var context = renderer.getContext();
+	context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
-// Connect it to the rendering context and draw!
-stave.setContext(context).draw();
+	// Create a stave of width 400 at position 10, 40 on the canvas.
+	var stave = new Vex.Flow.Stave(10, 40, 570); //Beeiflusst ebenfalls die Groeße
 
-var notes = [
-  // A quarter-note C.
-  new Vex.Flow.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
+	// Add a clef and time signature.
+	stave.addClef(clef);//.addTimeSignature("4/4"); 
 
-  // A quarter-note D.
-  new Vex.Flow.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
+	// Connect it to the rendering context and draw!
+	stave.setContext(context).draw();
 
-  // A quarter-note rest. Note that the key (b/4) specifies the vertical
-  // position of the rest.
-  new Vex.Flow.StaveNote({clef: "treble", keys: ["b/4"], duration: "q" }),
+	var notes = [
+		// A whole-note.
+		new Vex.Flow.StaveNote({clef: clef, keys: [note], duration: "w" })
+	];
 
-  // A C-Major chord.
-  new Vex.Flow.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
-];
+	// Create a voice in 1/1 and add above notes
+	var voice = new Vex.Flow.Voice({num_beats: 1,  beat_value: 1});
+	voice.addTickables(notes);
 
-// Create a voice in 4/4 and add above notes
-var voice = new Vex.Flow.Voice({num_beats: 4,  beat_value: 4});
-voice.addTickables(notes);
+	// Format and justify the notes to 400 pixels.
+	var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 400);
 
-// Format and justify the notes to 400 pixels.
-var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 400);
-
-// Render voice
-voice.draw(context, stave);
-
+	// Render voice
+	voice.draw(context, stave);
+}
 //Rendert (später) Notenanzeige
 function renderRenderbody(solutionIndex){
 	let textArr=JSON.parse(localStorage.getItem("notes"));
 	document.getElementById("renderParagraph").innerHTML= textArr[solutionIndex];
 }
-
-renderRenderbody(solutionIndex);
-
+//Überspring 2 und 3
 
 
 //---------------------------
@@ -151,9 +163,12 @@ let solutionValue = JSON.parse(localStorage.getItem("solution"));
 //Setzt nächsten Rendering Content zusammen/ wechselt zur nächsten Seite & summiert richtige Antworten
 function handleButton(event){
 	let h = JSON.parse(localStorage.getItem("solution"));
-
+	console.log(solutionIndex);
 	let lastPage = () => {
 		jP.innerHTML = Correct +"/4";
+		//let section ='<section id="main__statistic" class="hidden"><div id="statistic__container"><h2>Dein Ergebnis</h2><span id="Jens" style="text-align: center; font-size: 5rem"></span><button class="statistic__button" id="statistic__button">Zurück zum Menu </button></div></section>'
+		//document.getElementById("main").innerHTML=section;
+		solutionIndex=0;
 		quiz.classList.add("hidden");
 		stat.classList.remove("hidden");	
 	}
@@ -161,11 +176,10 @@ function handleButton(event){
 	let nextPage = () =>{
 		randomChoice = shuffle(randomChoice);
 		randomizeChoiceButtons(buttons, randomChoice);
-		solutionIndex+=1;
 		progBar.value += 10;
 		renderRenderbody(solutionIndex);
+		solutionIndex+=1;
 	}
-
 
 	if (event === solutionValue[solutionIndex]) {
 		if(solutionIndex === 3){
@@ -174,30 +188,57 @@ function handleButton(event){
 		}
 		else
 		{
-			Correct+=1; //BUG?
+			Correct+=1;
 			nextPage();
 		}
 	}
 	else{
-		if(solutionIndex === 3){lastPage();}
+		if(solutionIndex === 3){
+			lastPage();
+		}
 		else
-		{nextPage();}
+		{
+			nextPage();
+		}
 	}
 }
 
+function insertFirstPage(){
+	let section = '<section id="main__quiz" class=""><div class="quiz__progressbox"><progress id="progress" value="00" max="40" class="quiz__progressbar"></progress></div><div class="quiz__renderbody" id="quiz__renderbody"><p style="text-align:center; font-size:2rem;"  id="renderParagraph"></p><canvas id="renderbody"></canvas></div><hr><h2>Wählen sie eine Lösungsmöglichkeit aus</h2><div class="main__choicebox" id="main__choicebox"><button class="choiceButton1" id="choiceButton1">A</button><button class="choiceButton2" id="choiceButton2">B</button><button class="choiceButton3" id="choiceButton3">C</button><button class="choiceButton4" id="choiceButton4">D</button></div></section>'
+
+	//document.getElementById("main").innerHTML=section;
+	menu.classList.add("hidden");
+	quiz.classList.remove("hidden");
+	//renderRenderbody(solutionIndex);
+	randomizeChoiceButtons(buttons, randomChoice);
+	responseButtons();
+	solutionIndex+=0;
+}
+
+//fect('http://192.168.0.108:8000/Modul2.json');
 //Start Button
 document.getElementById("form_button").addEventListener('click', () =>{
 	var sel = document.getElementById("menuform").value;
-	if (sel !== 'Modul') {
-		menu.classList.add("hidden");
-		quiz.classList.remove("hidden");
-		renderRenderbody(solutionIndex);
+	const htwAddr1="https://www2.htw-dresden.de/~s77199/Modul1.json";
+	const htwAddr2="https://www2.htw-dresden.de/~s77199/Modul2.json";
+	const mod2Addr= 'http://192.168.0.108:8000/Modul2.json'; 
+	const mod1Addr= 'http://192.168.0.108:8000/Modul1.json'; 
+
+	if(sel === 'Bassschlüssel'){
+		localStorage.clear();
+		fetchRequest(htwAddr2);
+		renderNotes(1);
 	}
-	else
-	{
+	if(sel === 'Violinschlüssel'){
+		localStorage.clear();
+		fetchRequest(htwAddr1);
+		renderNotes(2);
+	}
+	if (sel === 'Modul') {
 		form.classList.add("main__choicebox--redLine");
 	}
-})
+	
+});
 
 //Restart Button
 document.getElementById("statistic__button").addEventListener('click', () =>{
@@ -211,7 +252,8 @@ document.getElementById("statistic__button").addEventListener('click', () =>{
 });
 
 // Quiz Buttons
-document.getElementById("main__choicebox").addEventListener('click', function(event){
+function responseButtons(){
+	document.getElementById("main__choicebox").addEventListener('click', function(event){
 	let elem = event.target.id;
 
 	if(elem === buttons[0]){
@@ -231,3 +273,22 @@ document.getElementById("main__choicebox").addEventListener('click', function(ev
 		handleButton(buttonText);
 	}
 });
+}
+
+/*
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+*/
+
+
+
+
